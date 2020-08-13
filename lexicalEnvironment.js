8b63dc0d441a753f653fb959c3b7f1bc0c7d5d66
Dasha <davydova_di@mail.ru>
2020-08-13 18:00:57 +0300

    fix: fuction|lexicalEnvironment.js

--- lexicalEnvironment.js
+++ lexicalEnvironment.js
@@ -2,17 +2,29 @@ let isNumber = function (n) {
     return !isNaN(parseFloat(n)) && isFinite(n);
 };
 
-function guessNum(){
-    const num =17;
+function guessNum() {
+    const num = 17;
+    let attempts = 10;
     let userNum = prompt('Угадайте число');
-    if (!isNumber(userNum)) {
-        userNum = prompt('Угадайте число');
-    }
+
     function user(b) {
-        if (num>b) {
-            console.log('Загаданное число меньше');
-        } else if (num<b) console.log('Загаданное число больше');
+        if (!isNumber(userNum)) {
+            userNum = prompt('Угадайте число');
+        }
+        attempts--;
+        if (num < b) {
+            console.log('Загаданное число меньше, осталось попыток: ', attempts);
+            userNum = prompt('Угадайте число');
+        } else if (num > b) {
+            console.log('Загаданное число больше, осталось попыток: ', attempts);
+            userNum = prompt('Угадайте число');
+        } else if (num == b) {
+            console.log('Поздравляю, Вы победили!');
+        }
+
     }
-    return user(userNum)
+    return user(userNum);
+
 }
+
 guessNum();
